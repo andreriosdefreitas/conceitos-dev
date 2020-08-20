e9ad107ba49841b4e28062cdfa4bf5e365eedc98
André Rios <andre.rios.freitas@gmail.com>
2020-08-20 12:05:28 +0100

    props

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 import Header from './components/Header'
 
 function App(){
-    return <Header/>;
+    return (
+        <>
+            <Header title="Homepage">
+                <ul>
+                    <li>A</li>
+                    <li>B</li>
+                </ul>
+            </Header>
+            <Header title="Projects">
+            <ul>
+                    <li>A</li>
+                    <li>B</li>
+                    <li>C</li>
+                </ul>
+            </Header>
+        </>
+    );
 }
 
 export default App;
