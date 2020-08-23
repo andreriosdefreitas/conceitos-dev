ff2d0fc412f9997953f616fe5b8e989df6f2159f
André Rios <andre.rios.freitas@gmail.com>
2020-08-23 13:05:08 +0100

    react-native add projects

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,30 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import {View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar} from 'react-native';
+import { View, SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 import api from './services/api';
 
-export default function App () {
+export default function App() {
     const [projects, setProjects] = useState([]);
 
-useEffect(() => {
-    api.get('projects').then(response => {
-        console.log(response.data);
-        setProjects(response.data);
-    });
-}, []);
+    useEffect(() => {
+        api.get('projects').then(response => {
+            console.log(response.data);
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `New project ${Date.now()}`,
+            owner: 'Andre'
+        });
+        setProjects([...projects, response.data]);
+    }
 
     return (
         <>
-            <StatusBar barStyle="light-content"/>
-            <SafeAreaView style = {styles.container}>
-                <FlatList 
-                    data = {projects}
-                    keyExtractor = {project => project.id}
-                    renderItem = { ({item: project}) => (
+            <StatusBar barStyle="light-content" />
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
                         <Text key={project.id} style={styles.project}>
                             {project.title}
                         </Text>
                     )}
                 />
+                <TouchableOpacity style={styles.button} onPress={handleAddProject}>
+                    <Text style={styles.buttonText}>
+                        Add project
+                    </Text>
+                </TouchableOpacity>
             </SafeAreaView>
             {/* <View style={styles.container}>
                 {projects.map(project =>( 
@@ -37,16 +50,28 @@ useEffect(() => {
     );
 }
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
         flex: 1, //faz ocupar a tela toda, sem isso, fica em branco pq o container ta vazio
-        backgroundColor: `#123`,
-        color: `#fff`
+        backgroundColor: '#123',
+        color: '#fff'
         // justifyContent: 'center',
         // alignItems: 'center'
     },
     project: {
-        color: `#fff`,
+        color: '#fff',
         fontSize: 20
+    },
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16
     }
 });
