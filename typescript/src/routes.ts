69e87ef8c299b3748dce2b03184bdea19f8da5e9
André Rios <andre.rios.freitas@gmail.com>
2020-08-24 21:11:05 +0100

    create user

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,11 @@
 import {Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+    const user = createUser({
+        email: 'email',
+        password: 'pass',
+        techs: ['A', 'B', {title: 'JS', experience: 100}]
+    });
     return response.json({nessage: 'Hello World'});
 }
